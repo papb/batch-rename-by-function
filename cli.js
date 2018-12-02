491db45e8703e85a5d929c1d3551609eb65816a2
papb <papb1996@gmail.com>
2018-12-01 23:30:25 -0200

    refactor: use yargs and fs-jetpack

--- cli.js
+++ cli.js
@@ -2,114 +2,65 @@
 /* eslint no-process-exit: off */
 "use strict";
 
-const VERSION = "1.1.0";
+const jetpack = require("fs-jetpack");
+const chalk = require("chalk");
+const yargs = require("yargs");
 
-const USAGE_TEXT = `    Usage
-    
-        $ batch-rename-by-function <js-file-which-exports-the-renaming-function>
-        
-        Takes the function exported by the JS file (by module.exports)
-        and applies it to every file present in the current folder,
-        including folders (with the exception of the passed JS file
-        itself, in case it is in the current folder).
+yargs.usage(`\nUsage: batch-rename-by-function path/to/my/renamer/file.js\n\n  Applies the function exported by the given JS file on every file present in the current folder, except the given JS file (if present), including folders.\n\n  The renaming function receives two parameters. The first is the name of the object (file/directory), and the second is a boolean which is true iff it is a directory. This way directories can be easily skipped, if desired.`);
+yargs.example("batch-rename-by-function myRenamer.js");
+// yargs.option("nested", {
+//     alias: "recursive",
+//     describe: "Whether to rename nested files",
+//     type: "boolean",
+//     default: false
+// });
+yargs.option("dry-run", {
+    describe: "Show which renames would happen without actually performing them",
+    type: "boolean",
+    default: false
+});
+yargs.check(argv => {
+    argv.dryRun = argv["dry-run"];
 
-        The renaming function receives two parameters. The first is the name
-        of the object (file/directory), and the second is a boolean which is
-        true iff it is a directory. This way directories can be easily skipped.
+    // Check positionals
+    const fileName = argv._[0];
+    if (!fileName) throw new Error("Missing argument (path to JS file).");
+    const fileExists = jetpack.exists(fileName);
+    if (!fileExists) throw new Error("Invalid argument: file not found.");
+    if (fileExists !== "file") throw new Error("Invalid argument: not a file.");
+    argv.fileName = fileName.endsWith(".js") ? fileName : fileName + ".js";
+    try {
+        argv.renamer = require(jetpack.path(fileName));
+    } catch (e) {
+        throw new Error(`Unable to require("${jetpack.path(fileName)}").`);
+    }
+    if (typeof argv.renamer !== "function") {
+        throw new Error("The JS file was found and processed by NodeJS but it did not export a function.");
+    }
 
-    Options
-    
-        --force, -F
-
-            Perform the actual renaming (without it, for safety, just
-            a preview is made)
-
-        --version
-
-        --help
-
-    Examples
-    
-        $ batch-rename-by-function myRenamer.js
-        $ batch-rename-by-function -F myRenamer.js`;
+    return true;
+});
+const args = yargs.argv;
 
 // ------------------ LOG & ERROR WRAPPERS ------------------ //
 
-function say(str) {
-    if (say.notTheFirstTime) {
-        console.log("\n" + str);
-    } else {
-        say.notTheFirstTime = true;
-        console.log("\n" + str);
-    }
+function print(str, color) {
+    console.log("\n" + (color ? chalk[color](str) : str));
 }
 function errorDontWorryExit(message) {
-    say(chalk.red("Error: " + message));
-    say("Try: batch-rename-by-function --help");
-    say(chalk.yellow("Don't worry. All files remained unchanged."));
+    print(`Error: ${message}`, "red");
+    print(`Try: batch-rename-by-function --help`);
+    print(`Don't worry. All files remained unchanged.`, "yellow");
     process.exit(1);
 }
 
-// ------------------ IMPORTS ------------------ //
-
-const jetpack = require('fs-jetpack');
-const chalk = require('chalk');
-
-// ------------------ PARSE ARGS ------------------ //
-
-const [,,...args] = process.argv;
-
-if (!args.length || (args.length === 1 && (args[0] === "--help" || args[0] === '-h'))) {
-    say(USAGE_TEXT);
-    process.exit();
-}
-
-if (args.length === 1 && (args[0] === "--version" || args[0] === '-v')) {
-    say("batch-rename-by-function v" + VERSION);
-    process.exit();
-}
-
-if (args.length > 2) {
-    errorDontWorryExit("Too many args.");
-}
-
-let renamingFunction = undefined;
-let arg = undefined;
-let force = false;
-
-if (args.length === 1) {
-    arg = args[0];
-} else if (args[0] === "--force" || args[0] === "-F") {
-    arg = args[1];
-    force = true;
-} else if (args[1] === "--force" || args[1] === "-F") {
-    arg = args[0];
-    force = true;
-} else if (args.length === 2) {
-    errorDontWorryExit("Error: Unable to parse args.");
-}
-
-try {
-    renamingFunction = require(arg);
-} catch (e) {
-    errorDontWorryExit(`Unable to require("${arg}").`);
-}
-
-if (typeof renamingFunction !== "function") {
-    errorDontWorryExit("The JS file was found and processed by NodeJS but it did not export a function.");
-}
-
-// ------------------ DONE CHECKING ARGS ------------------ //
-
-say(chalk.green("Command inputs OK."));
-
-if (force) {
-    say(chalk.yellow("In '--force' mode: files will be renamed."));
+if (args.dryRun) {
+    print(`In '--dry-run' mode: files will not be really renamed (only a preview will be given).`, "green");
 } else {
-    say(chalk.green("Not in '--force' mode: files will not be renamed, only a preview will be given."));
+    print(`Not in '--dry-run' mode: files will be really renamed.`, "yellow");
 }
 
-say(chalk.yellow("Reminder: folders themselves are included in the process!"));
+print(`Reminder: folders themselves are included in the process!`, "yellow");
 
 // ------------------ READ THINGS ------------------ //
 
@@ -119,62 +70,61 @@ let totalAmount = 0;
 let amountUnaltered = 0;
 
 // Compute changes
-fileNames.forEach(name => {
-
-    if (name === args[0] || name === args[0] + ".js") {
-        say(chalk.green(`Skipping file: ${name}.`));
-        return;
+for (const name of fileNames) {
+    if (name === args.fileName) {
+        print(`Skipping file: ${name}.`, "green");
+        continue;
     }
 
     const isDirectory = jetpack.exists(name) === "dir";
-
-    const renamed = renamingFunction(name, isDirectory);
+    const renamed = args.renamer(name, isDirectory);
 
     if (typeof renamed !== "string") {
-        errorDontWorryExit(`The Renaming Function returned non-string for the input "${name}"!`);
+        errorDontWorryExit(`The renaming function returned non-string for the input "${name}"!`);
     }
     if (renamed.length === 0) {
-        errorDontWorryExit(`The Renaming Function returned empty string for the input "${name}"!`);
+        errorDontWorryExit(`The renaming function returned empty string for the input "${name}"!`);
     }
 
-    renameList.push({
-        old: name,
-        new: renamed
-    });
+    renameList.push({ old: name, new: renamed });
 
     totalAmount++;
     if (name === renamed) amountUnaltered++;
-
-});
+}
 
 // ------------------ DO THE MAGIC ------------------ //
 
 if (amountUnaltered > 0) {
-    say(chalk.green("Unaltered files:"));
+    print("Unaltered files:", "green");
     renameList.filter(rename => rename.old === rename.new).forEach(rename => {
         console.log(`    ${rename.old}`);
     });
 }
 if (totalAmount > amountUnaltered) {
-    say(chalk.yellow(force ? "Altered files:" : "Files to be altered:") + "\n");
-    renameList.filter(rename => rename.old !== rename.new).forEach(rename => {
-        if (force) jetpack.rename(rename.old, rename.new);
+    print(`${args.dryRun ? "Files to be altered" : "Altered files"}:\n`, "yellow");
+    const toBeAltered = renameList.filter(rename => rename.old !== rename.new);
+    for (const rename of toBeAltered) {
+        if (!args.dryRun) jetpack.rename(rename.old, rename.new);
         console.log(`    "${rename.old}" -> "${rename.new}".`);
-    });
+    }
 }
 
 // ------------------ COMMAND SUMMARY ------------------ //
 
-if (force) {
-    say(chalk.blue(`Finished renaming.`));
-    console.log(`\n    Total files: ${totalAmount}.`);
-    console.log(`    Altered files: ${totalAmount - amountUnaltered}.`);
-    console.log(`    Unaltered files: ${amountUnaltered}.`);
+if (args.dryRun) {
+    print(`Preview finished.`, "blue");
+    print([
+        `    Total files: ${totalAmount}.`,
+        `    Files to be altered: ${totalAmount - amountUnaltered}.`,
+        `    Files that will remain unaltered: ${amountUnaltered}.`
+    ].join("\n"));
 } else {
-    say(chalk.blue(`Preview finished.`));
-    console.log(`\n    Total files: ${totalAmount}.`);
-    console.log(`    Files to be altered: ${totalAmount - amountUnaltered}.`);
-    console.log(`    Files that will remain the same: ${amountUnaltered}.`);
+    print(`Finished renaming.`, "blue");
+    print([
+        `    Total files: ${totalAmount}.`,
+        `    Altered files: ${totalAmount - amountUnaltered}.`,
+        `    Unaltered files: ${amountUnaltered}.`
+    ].join("\n"));
 }
 
 // ----------------------------------------------------- //
